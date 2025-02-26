81159f2e075e681801c09e2417ecd573068f7392
four-kilos <prjn0022@gmail.com>
2025-02-25 22:00:09 -0500

    Add files via upload

---
+++ v2/service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "notes-logger-v1";
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./styles.css",
+    "./script.js",
+    "./manifest.json",
+    "./icons/icon-192x192.png",
+    "./icons/icon-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
